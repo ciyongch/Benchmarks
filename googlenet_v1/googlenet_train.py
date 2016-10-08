196c0f574e7b2b436d46463c27a41edccf1866d3
Ciyong Chen <ciyong.chen@intel.com>
2016-10-08 22:54:30 +0800

    update params_save and params_load function in googlenet

--- googlenet_v1/googlenet_train.py
+++ googlenet_v1/googlenet_train.py
@@ -6,7 +6,7 @@ import theano.tensor as T
 import numpy as np
 import cPickle as pickle
 from googlenet_theano import googlenet, compile_models, set_learning_rate
-from params import get_params, load_net_state, load_params, save_net_state, save_figure
+from params import get_params, load_net_state, load_params, save_net_state
 from read_lmdb import read_lmdb
 
 def googlenet_train(batch_size=32, image_size=(3, 224, 224), n_epochs=60, mkldnn=False):
@@ -33,13 +33,14 @@ def googlenet_train(batch_size=32, image_size=(3, 224, 224), n_epochs=60, mkldnn
         shared_x, shared_y, shared_lr, vels) = compile_models(model, batch_size = batch_size)
 
     ####load net state
-    net_params = load_net_state()
+    start_epoch = 0
+    net_params = load_net_state(start_epoch)
     if net_params:
         load_params(model.params, net_params['model_params'])
         load_params(vels, net_params['vels'])
-        train_lmdb_iterator.set_cursor(net_params['minibatch_index'])
         epoch = net_params['epoch']
         minibatch_index = net_params['minibatch_index']
+        train_lmdb_iterator.set_cursor(net_params['minibatch_index'])
     else:
         epoch = 0
         minibatch_index = 0
@@ -71,7 +72,7 @@ def googlenet_train(batch_size=32, image_size=(3, 224, 224), n_epochs=60, mkldnn
             if idx % 40 == 0:
                 average_cost = np.array(cost_array).mean()
                 cost_array = []
-                print('iter %d, cost %f, error_ij' % (idx, average_cost, error_ij)) #FIXME, mean loss for googlenet
+                print('iter %d, cost %f, error_ij %f' % (idx, average_cost, error_ij)) #FIXME, mean loss for googlenet
             minibatch_index += 1
             if minibatch_index == n_train_batches:
                 minibatch_index = 0
@@ -83,24 +84,23 @@ def googlenet_train(batch_size=32, image_size=(3, 224, 224), n_epochs=60, mkldnn
 
         for validation_index in xrange(0, n_val_batches):
             val_data, val_label = val_lmdb_iterator.next()
-            val_shared_x.set_value(val_data)
-            val_shared_y.set_value(val_label)
-            cost, errors, errors_top_5 = val_model()
+            shared_x.set_value(val_data)
+            shared_y.set_value(val_label)
+            cost, errors, errors_top_5 = validate_model()
             val_top5_errors.append(errors_top_5)
             val_top1_errors.append(errors)
         model.set_dropout_on()
         val_top5_err = np.mean(val_top5_errors)
         val_top1_err = np.mean(val_top1_errors)
-        print('epoch %i, minibatch %i/%i, validation error (top1) %f %%, (top5) %f %%' %
-              (epoch, minibatch_index + 1, n_train_batches,
-               val_top5_err * 100., val_top1_err * 100.))
+        print('epoch %i, validation error (top1) %f %%, (top5) %f %%' %
+              (epoch, val_top5_err * 100., val_top1_err * 100.))
 
         ###save params every epoch
         net_params['model_params'] = get_params(model.params)
         net_params['vels'] = get_params(vels)
         net_params['minibatch_index'] = minibatch_index
         net_params['epoch'] = epoch
-        save_net_state(net_params)
+        save_net_state(net_params, epoch)
 
         epoch = epoch + 1
 
